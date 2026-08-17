8070f41bfe9d1359d91aff3c489b679d403e24ae
abbasjari1234-dotcom <abbasjari1234@gmail.com>
2026-08-17 21:17:52 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body className="bg-obsidian-950 text-cream font-sans antialiased min-h-screen">
         {children}
         <CartDrawer />
